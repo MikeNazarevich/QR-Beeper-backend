fa303d79ce6e52be4828138be472e054afeb09e8
M.Nazarevich <mikhail.nazarevich@docomodigital.com>
2023-03-19 14:39:45 +0100

    initial commit

---
+++ src/main/java/com/dungeon/beeper/QrBeeperApplication.java
@@ -0,0 +1,13 @@
+package com.dungeon.beeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrBeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrBeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dungeon/beeper/QrBeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungeon.beeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrBeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
